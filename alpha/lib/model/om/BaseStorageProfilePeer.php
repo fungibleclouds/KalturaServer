8fc1ff318a73d3ca88f450f666ede3a989105756
Rotem Arel <rotem.arel@kaltura.com>
2012-11-12 15:33:09 +0000

    qnd: qc 4750 added support for Amazon S3 storage profile
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/gemini_2012_10_22@90103 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/om/BaseStorageProfilePeer.php
+++ alpha/lib/model/om/BaseStorageProfilePeer.php
@@ -774,6 +774,9 @@ abstract class BaseStorageProfilePeer {
 				// $obj->hydrate($row, 0, true); // rehydrate
 				$results[] = $obj;
 			} else {
+				// class must be set each time from the record row
+				$cls = StorageProfilePeer::getOMClass($row, 0);
+				$cls = substr('.'.$cls, strrpos('.'.$cls, '.') + 1);
 				$obj = new $cls();
 				$obj->hydrate($row);
 				$results[] = $obj;
@@ -807,21 +810,27 @@ abstract class BaseStorageProfilePeer {
 	}
 
 	/**
-	 * The class that the Peer will make instances of.
+	 * The returned Class will contain objects of the default type or
+	 * objects that inherit from the default.
 	 *
-	 * If $withPrefix is true, the returned path
-	 * uses a dot-path notation which is tranalted into a path
-	 * relative to a location on the PHP include_path.
-	 * (e.g. path.to.MyClass -> 'path/to/MyClass.php')
-	 *
-	 * @param      boolean  Whether or not to return the path wit hthe class name 
-	 * @return     string path.to.ClassName
+	 * @param      array $row PropelPDO result row.
+	 * @param      int $colnum Column to examine for OM class information (first is 0).
+	 * @throws     PropelException Any exceptions caught during processing will be
+	 *		 rethrown wrapped into a PropelException.
 	 */
-	public static function getOMClass($withPrefix = true)
+	public static function getOMClass($row, $colnum)
 	{
-		return $withPrefix ? StorageProfilePeer::CLASS_DEFAULT : StorageProfilePeer::OM_CLASS;
-	}
+		try {
 
+			$omClass = $row[$colnum + 8];
+			$omClass = substr('.'.$omClass, strrpos('.'.$omClass, '.') + 1);
+
+		} catch (Exception $e) {
+			throw new PropelException('Unable to get OM class.', $e);
+		}
+		return $omClass;
+	}
+	
 	/**
 	 * Method perform an INSERT on the database, given a StorageProfile or Criteria object.
 	 *
