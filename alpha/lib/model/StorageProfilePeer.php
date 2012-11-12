0c1cf0cd881732c74565a28247c54db7a394aaaa
Rotem Arel <rotem.arel@kaltura.com>
2012-11-12 15:33:57 +0000

    qnd: qc 4750 added support for Amazon S3 storage profile
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/gemini_2012_10_22@90104 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/lib/model/StorageProfilePeer.php
+++ alpha/lib/model/StorageProfilePeer.php
@@ -10,6 +10,49 @@
  */ 
 class StorageProfilePeer extends BaseStorageProfilePeer
 {
+	const STORAGE_DEFAULT_OM_CLASS = 'StorageProfile';
+	const STORAGE_AMAZON_S3_OM_CLASS = 'AmazonS3StorageProfile';
+	
+	// cache classes by their type
+	protected static $class_types_cache = array(
+		StorageProfile::STORAGE_PROTOCOL_FTP => self::STORAGE_DEFAULT_OM_CLASS,
+		StorageProfile::STORAGE_PROTOCOL_SCP => self::STORAGE_DEFAULT_OM_CLASS,
+		StorageProfile::STORAGE_PROTOCOL_SFTP => self::STORAGE_DEFAULT_OM_CLASS,
+		StorageProfile::STORAGE_PROTOCOL_SFTP_CMD => self::STORAGE_DEFAULT_OM_CLASS,
+		StorageProfile::STORAGE_PROTOCOL_SFTP_SEC_LIB => self::STORAGE_DEFAULT_OM_CLASS,
+		StorageProfile::STORAGE_PROTOCOL_S3 => self::STORAGE_AMAZON_S3_OM_CLASS,
+	);
+	
+	/**
+	 * The returned Class will contain objects of the default type or
+	 * objects that inherit from the default.
+	 *
+	 * @param      array $row PropelPDO result row.
+	 * @param      int $colnum Column to examine for OM class information (first is 0).
+	 * @throws     PropelException Any exceptions caught during processing will be
+	 *		 rethrown wrapped into a PropelException.
+	 */
+	public static function getOMClass($row, $colnum)
+	{
+		if($row)
+		{
+			$protocolField = self::translateFieldName(StorageProfilePeer::PROTOCOL, BasePeer::TYPE_COLNAME, BasePeer::TYPE_NUM);
+			$storageProfileProtocol = $row[$protocolField];
+			if(isset(self::$class_types_cache[$storageProfileProtocol]))
+				return self::$class_types_cache[$storageProfileProtocol];
+				
+			$extendedCls = KalturaPluginManager::getObjectClass(parent::OM_CLASS, $storageProfileProtocol);
+			if($extendedCls)
+			{
+				self::$class_types_cache[$storageProfileProtocol] = $extendedCls;
+				return $extendedCls;
+			}
+			self::$class_types_cache[$storageProfileProtocol] = parent::OM_CLASS;
+		}
+			
+		return parent::OM_CLASS;
+	}
+	
 	public static function alternativeCon($con, $queryDB = kQueryCache::QUERY_DB_UNDEFINED)
 	{
 		if($con === null)
