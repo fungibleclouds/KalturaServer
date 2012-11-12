5471276b72562bb02fd0570a74c7a7e62d3e04d0
Rotem Arel <rotem.arel@kaltura.com>
2012-11-12 15:27:00 +0000

    qnd: qc 4750 added support for Amazon S3 storage profile
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/gemini_2012_10_22@90100 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/model/kStorageExportJobData.php
+++ alpha/apps/kaltura/lib/batch2/model/kStorageExportJobData.php
@@ -9,6 +9,11 @@ class kStorageExportJobData extends kStorageJobData
 	 * @var bool
 	 */   	
     private $force;
+    
+    /**
+	 * @var KalturaAmazonS3StorageProfileFilesPermissionLevel
+	 */   	
+    private $filesPermissionInS3;
 	
 	/**
 	 * @return the $force
@@ -24,6 +29,22 @@ class kStorageExportJobData extends kStorageJobData
 	public function setForce($force)
 	{
 		$this->force = $force;
+	}
+	
+	/**
+	 * @return the $filesPermissionInS3
+	 */
+	public function getFilesPermissionInS3()
+	{
+		return $this->filesPermissionInS3;
+	}
+	
+	/**
+	 * @param $filesPermissionInS3 the $filesPermissionInS3 to set
+	 */
+	public function setFilesPermissionInS3 ($filesPermissionInS3)
+	{
+		$this->filesPermissionInS3 = $filesPermissionInS3;	
 	}
 
 }
